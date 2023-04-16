940cd595ecb6256a955bd68f806708650292683d
testuser1 <tushardusane10@gmail.com>
2023-04-16 23:29:57 +0530

    first commit

---
+++ src/test/java/com/example/javapipeline/JavaPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javapipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
